ed16e6ec66cb6dcb7665b2541b82960e15e148d8
mastertom <mastertom0306@gmail.com>
2014-03-17 17:15:16 -0500

    leetcode

--- Regular Expression Matching/RegularEM.cpp
+++ Regular Expression Matching/RegularEM.cpp
@@ -1,19 +1,29 @@
 #include <iostream>
+#include <cassert>
 using namespace std;
 
 class Solution {
 public:
     bool isMatch(const char *s, const char *p) {
-
-        return true;
+        assert(s && p);
+        if (*p == '\0') return *s == '\0';
+        if (*(p+1) != '*')
+        {
+            assert(*p!='*');
+            return((*p == *s) || (*p == '.' && *s != '\0')) && isMatch(s+1,p+1);
+        }
+        while((*p == *s) || (*p == '.' && *s != '\0')){
+            if (isMatch(s,p+2)) return true;
+            s++;}
+        return isMatch(s,p+2);
     }
 };
 
 int main()
 {
     Solution SO;
-    char s[] = "aaa";
-    char p[] = "a*a";
-    cout<<isMatch(s,p)<<endl;
+    char s[] = "b";
+    char p[] = "b*bb";
+    cout<<SO.isMatch(s,p)<<endl;
     return 0;
 }
